a6569376dd9c0bcad8320fbd1a0ae8b73189f6c2
vicblanc92 <vicbr92@gmail.com>
2022-04-13 14:01:39 +0200

    create users.js and get all users

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+const router = express.Router();
+
+let users = [];
+
+router.get('/', (req, res) => {
+  res.send(users);
+});
+
+export default router;
